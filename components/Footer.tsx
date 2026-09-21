8c71bba05d2a624c686afa6a04b87cad6ca25cb5
TacitusKilgore96 <collywobb@gmail.com>
2026-09-21 13:34:16 +0200

    Cena Smash

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mx-auto flex-col w-full'>
+      <div className='bg-[#434343]'>
+        Upper Footer
+      </div>
+      <div className='bg-[#1a1a1a]'>
+        Bottom Footer
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-[#ffffffff] text-black'>
+      Header
+    </div>
+  )
+}
+
+export default Header
